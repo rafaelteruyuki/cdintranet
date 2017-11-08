c31c05f14230a2c6d93e6837a8129c2948f28a01
Rafael Franchin <rafael.franchin@gmail.com>
2017-11-08 17:37:24 -0200

    Otimização carregamento

--- template-parts/var-tarefas.php
+++ template-parts/var-tarefas.php
@@ -21,13 +21,13 @@ $imagem_miniatura = get_field('imagem_miniatura');
 $imagem_rodape = get_field('imagem_rodape');
 $imagem_lateral = get_field('imagem_lateral');
 
-$count_naoiniciado = get_post_count_by_meta('status', 'naoiniciado', 'tarefa');
-$count_cancelado = get_post_count_by_meta('status', 'cancelado', 'tarefa');
-$count_incompleto = get_post_count_by_meta('status', 'incompleto', 'tarefa');
-$count_aguardando = get_post_count_by_meta('status', 'aguardandoinformacao', 'tarefa');
-$count_emproducao = get_post_count_by_meta('status', 'emproducao', 'tarefa');
-$count_publicado = get_post_count_by_meta('status', 'publicado', 'tarefa');
-$count_finalizado = get_post_count_by_meta('status', 'finalizado', 'tarefa');
+// $count_naoiniciado = get_post_count_by_meta('status', 'naoiniciado', 'tarefa');
+// $count_cancelado = get_post_count_by_meta('status', 'cancelado', 'tarefa');
+// $count_incompleto = get_post_count_by_meta('status', 'incompleto', 'tarefa');
+// $count_aguardando = get_post_count_by_meta('status', 'aguardandoinformacao', 'tarefa');
+// $count_emproducao = get_post_count_by_meta('status', 'emproducao', 'tarefa');
+// $count_publicado = get_post_count_by_meta('status', 'publicado', 'tarefa');
+// $count_finalizado = get_post_count_by_meta('status', 'finalizado', 'tarefa');
 
 $modificado = get_the_modified_time('dmYGis');
 $atual = date( 'd/m/Y G:i', current_time( 'timestamp', 0 ) );
