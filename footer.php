c1ddbca4dd4c5c84cd1db6ea5ff9114994ac74dd
Rafael Franchin <rafael.franchin@gmail.com>
2017-12-19 18:52:39 -0200

    Funcionando

--- footer.php
+++ footer.php
@@ -100,6 +100,40 @@ function fnExcelReport() {
 
 </script>
 
+<div class="ui tiny modal cd-natal" style="width: 580px;margin: 0 0 0 -290px">
+  <i class="close icon"></i>
+  <div class="image content">
+    <img src="<?php bloginfo('template_url'); ?>/images/natal2.gif">
+  </div>
+  <div class="actions">
+    <a class="ui cd-natal-obrigado button" href="javascript:dontShow()">Não mostrar novamente</a>
+  </div>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js"></script>
+
+<script type="text/javascript">
+
+// Cookies.set('mostrar', 'yes', { expires: 7 });
+// Cookies.remove('mostrar');
+
+function dontShow() {
+  Cookies.set('mostrar', 'no', { expires: 7 });
+}
+
+var mostrar = Cookies.get('mostrar');
+
+if (mostrar !== 'no') {
+  $('.modal.cd-natal')
+    .modal('attach events', '.cd-natal-obrigado', 'hide')
+    .modal('show')
+  ;
+}
+
+alert(mostrar);
+
+</script>
+
 <!-- Analytics -->
 
 <script>
